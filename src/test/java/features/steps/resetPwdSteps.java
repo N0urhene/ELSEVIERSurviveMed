1889ce2e7bc501df883b6a2eed0728d1d224fad1
Nourhene Soueid <76226223+N0urhene@users.noreply.github.com>
2023-05-17 12:16:27 +0100

    adding reset password steps

---
+++ src/test/java/features/steps/resetPwdSteps.java
@@ -0,0 +1,48 @@
+package features.steps;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import utility.Hook;
+
+public class resetPwdSteps {
+    private AndroidDriver driver;
+    public resetPwdSteps() {this.driver = Hook.getDriver();}
+
+    @Given("The user is in the login webpage")
+    public void the_user_is_in_the_login_webpage() {
+
+    }
+
+    @When("The user click on Forgot Password link")
+    public void the_user_click_on_forgot_password_link() {
+        driver.findElement(By.xpath("//android.view.View[@content-desc=\"Forgot Password?\"]/android.widget.TextView")).click();
+    }
+
+    @When("The user is in the Reset Password screen")
+    public void the_user_is_in_the_reset_password_screen() {
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[1]/android.widget.TextView")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.widget.FrameLayout[2]/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View[1]/android.view.View[1]/android.widget.TextView")).isDisplayed());
+
+    }
+
+    @When("The user enter his email address in the address field")
+    public void the_user_enter_his_email_address_in_the_address_field() {
+        driver.findElement(By.id("1-email")).sendKeys("soueidnourhene@gmail.com");
+    }
+
+    @When("The user click on the blue button Send email")
+    public void the_user_click_on_the_blue_button_send_email() {
+        driver.findElement(By.id("1-submit")).click();
+
+    }
+
+    @Then("The user should receive a success message")
+    public void the_user_should_receive_a_success_message() {
+
+    }
+
+}
